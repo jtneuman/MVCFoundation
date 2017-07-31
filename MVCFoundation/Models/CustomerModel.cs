4cbc5ce99d6d443684fd8887187ed056b3a3f7bb
Jeff Neumann <Jeff.Neumann@phoenix.edu>
2017-07-31 15:17:09 -0400

    Exercise 2 complete.

---
+++ MVCFoundation/Models/CustomerModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCFoundation.Models
+{
+    public class CustomerModel
+    {
+        [Display(Name = "First name")]
+        [MinLength(5)]
+        public string FirstName { get; set; }
+
+        [EmailAddress]
+        [Required]
+        public string EmailAddress { get; set; }
+
+    }
+}
